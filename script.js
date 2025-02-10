8b0b82b723b8ad1dc9dd38c973674f16c87b0a8b
Through2Dexter <162742084+Through2Dexter@users.noreply.github.com>
2025-02-10 22:27:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // Selecting Elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -11,6 +13,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   //1. Generating random dice roll
@@ -19,4 +23,12 @@ btnRoll.addEventListener("click", function () {
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
   // 3. Check for rolled 1: if true, switch to next player
+
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore = currentScore + dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    //switch to next player
+  }
 });
